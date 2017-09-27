e426acc58b21f3dc185bcb395da70b4444060184
Kevin B <kevin.t.bradbury@gmail.com>
2017-09-26 19:08:47 -0700

    Successfully setup photo Upload to Firebase Storage
    Successfully setup photo Upload to Firebase Storage
    - currently hardcoded image name with date, storename, & PO number.
    - will OVERWRITE its existing file of same name anytime a photo is uploaded

--- FieldApp/FieldApp/HomeView.swift
+++ FieldApp/FieldApp/HomeView.swift
@@ -9,16 +9,30 @@
 import Foundation
 import UIKit
 import Firebase
+import FirebaseStorage
+import CoreLocation
 
-class HomeView: UIViewController {
-    
-    let firebaseAuth = Auth.auth()
+class HomeView: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var homeButton: UIButton!
     @IBOutlet weak var logoutButton: UIButton!
+    @IBOutlet weak var photoToUpload: UIImageView!
+    @IBOutlet weak var choosePhotoButton: UIButton!
+    
+    let firebaseAuth = Auth.auth()
+    let picker = UIImagePickerController()
+    var jobs: [Job.UserJob] = []
+    let main = OperationQueue.main
+    var location = UserData.init().userLocation
+    var jobAddress = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        picker.delegate = self
+        getJobs() { jobs in
+            self.jobs = jobs
+            self.checkJobProximity()
+        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -41,5 +55,137 @@ class HomeView: UIViewController {
         }
     }
 
+    @IBAction func chooseUploadMethod(_ sender: Any) {
+        showUploadMethods()
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let selectedPhoto = info[UIImagePickerControllerOriginalImage] as! UIImage
+        //let imagePath = info[UIImagePickerControllerReferenceURL]
+        
+        photoToUpload.contentMode = .scaleAspectFit
+        photoToUpload.image = selectedPhoto
+        
+        self.createPhotoRef(photo: selectedPhoto)
+        
+        dismiss(animated: true)
+    }
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
+
+extension HomeView {
+    
+    func showUploadMethods() {
+        
+        var actionsheet = UIAlertController(title: "Choose Upload method", message: "You can upload by Camera or from yourimageStorageRef Photos", preferredStyle: UIAlertControllerStyle.actionSheet)
+        
+        let chooseCamera = UIAlertAction(title: "Camera", style: UIAlertActionStyle.default) { (action) -> Void in
+            //present Camera
+            self.picker.allowsEditing = false
+            self.picker.sourceType = .camera
+            self.picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .camera)!
+            self.present(self.picker, animated: true, completion: nil)
+        }
+        let choosePhotos = UIAlertAction(title: "Photos", style: UIAlertActionStyle.default) { (action) -> Void in
+            //present Photos
+            self.picker.allowsEditing = false
+            self.picker.sourceType = .photoLibrary
+            self.picker.mediaTypes = UIImagePickerController.availableMediaTypes(for: .photoLibrary)!
+            self.present(self.picker, animated: true, completion: nil)
+        }
+        let cancel = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.destructive) { (action) -> Void in
+            //Removes photo from upload
+            print("chose Cancel")
+        }
+        actionsheet.addAction(chooseCamera)
+        actionsheet.addAction(choosePhotos)
+        actionsheet.addAction(cancel)
+        
+        self.present(actionsheet, animated: true)
+    }
+    func createPhotoRef(photo: UIImage) {
+        
+        guard let imageData = UIImageJPEGRepresentation(photo, 0.5) else {
+            print("Could not get JPEG representation of UIImage")
+            return
+        }
+        
+        let storage = Storage.storage()
+        let data = imageData
+        let storageRef = storage.reference()
+        
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM.dd.yyyy"
+        let result = formatter.string(from: date)
+        print("\n imageName will be: image\(result)\(jobs[1].storeName)\(jobs[1].poNumber).jpg")
+
+        let imageStorageRef = storageRef.child("image\(result)\(jobs[0].storeName)\(jobs[0].poNumber).jpg")
+        
+        let uploadTask = imageStorageRef.putData(data, metadata: nil) { (metadata, error) in
+            
+            guard let metadata = metadata else {
+                print("uploadtask error \(error)")
+                return
+            }
+            if error == nil {
+                let downloadURL = metadata.downloadURL()
+            }
+        }
+        uploadTask.enqueue()
+    }
+    
+}
+
+extension HomeView {
+    
+    func getJobs(callback: @escaping ([Job.UserJob]) -> ()) {
+        
+        APITestCall().fetchJobInfo() { jobs in
+            self.jobs = jobs
+            callback(jobs)
+            
+            UIApplication.shared.isNetworkActivityIndicatorVisible = true
+            
+            self.main.addOperation {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
+            print("Index 0 Job is --> \(self.jobs[0].jobName) \n")
+        }
+    }
+    
+    func getLocation(completition: @escaping (CLLocationCoordinate2D) -> Void) {
+        
+        UserLocation.instance.requestLocation(){ coordinate in
+            
+            self.location = coordinate
+            print("User location is --> \(coordinate) \n")
+
+            completition(coordinate)
+        }
+    }
+    
+    func checkJobProximity() {
+        
+        self.getLocation() { completition in
+            
+            self.jobAddress = "\(self.jobs[0].jobAddress), \(self.jobs[0].jobCity), \(self.jobs[0].jobState)"
+            GeoCoding.locationForAddressCode(address: self.jobAddress) { location in
+                let distance = GeoCoding.getDistance(userLocation: self.location!, jobLocation: location!)
+                print("Miles from job location is --> \(distance) \n")
+                
+                if distance > 1.0 {
+                    print("NO <-- User is not in proximity to Job location \n")
+                } else {
+                    print("YES <-- User is in proximity to Job location \n")
+                }
+            }
+        }
+        
+    }
 }
--- FieldApp/FieldApp/LoginViewController.swift
+++ FieldApp/FieldApp/LoginViewController.swift
@@ -16,19 +16,13 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var phoneNumberFIeld: UITextField!
     @IBOutlet weak var loginButton: UIButton!
     
-    var jobs: [Job.UserJob] = []
-    let main = OperationQueue.main
-    var location = UserData.init().userLocation
-    var jobAddress = ""
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         UserLocation.instance.initialize()
-        getJobs() {jobs in
-            self.checkJobProximity()
-        }
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -45,55 +39,6 @@ class LoginViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    func getJobs(callback: @escaping ([Job.UserJob]) -> ()) {
-        
-        APITestCall().fetchJobInfo() { jobs in
-            self.jobs = jobs
-            callback(jobs)
-            
-            UIApplication.shared.isNetworkActivityIndicatorVisible = true
-            
-            self.main.addOperation {
-                UIApplication.shared.isNetworkActivityIndicatorVisible = false
-            }
-            print("Index 0 Job is --> \(self.jobs[0].jobName) \n")
-        }
-    }
-    
-    func getLocation(completition: @escaping (CLLocationCoordinate2D) -> Void) {
-        
-        UserLocation.instance.requestLocation(){ coordinate in
-            
-            self.location = coordinate
-            
-//            if (self.location?.latitude)! > CLLocationDegrees(0.0) {
-                print("User location is --> \(coordinate) \n")
-//            } else {
-//                print("location failed")
-//            }
-            completition(coordinate)
-        }
-    }
-    
-    func checkJobProximity() {
-        
-        self.getLocation() { completition in
-            
-            self.jobAddress = "\(self.jobs[0].jobAddress), \(self.jobs[0].jobCity), \(self.jobs[0].jobState)"
-            GeoCoding.locationForAddressCode(address: self.jobAddress) { location in
-                let distance = GeoCoding.getDistance(userLocation: self.location!, jobLocation: location!)
-                print("Miles from job location is --> \(distance) \n")
-                
-                if distance > 1.0 {
-                    print("NO <-- User is not in proximity to Job location \n")
-                } else {
-                    print("YES <-- User is in proximity to Job location \n")
-                }
-            }
-        }
-        
     }
     
     @IBAction func loginPressed(_ sender: Any) {
