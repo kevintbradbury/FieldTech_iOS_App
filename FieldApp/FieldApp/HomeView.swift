0023efd3c38a2ae350e8924027750f1c2f0222c4
Kevin B <kevin.t.bradbury@gmail.com>
2017-12-26 17:16:22 -0800

    still trying to get image up to server

--- FieldApp/FieldApp/HomeView.swift
+++ FieldApp/FieldApp/HomeView.swift
@@ -67,20 +67,23 @@ class HomeView: UIViewController, UIImagePickerControllerDelegate, UINavigationC
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
         let selectedPhoto = info[UIImagePickerControllerOriginalImage] as! UIImage
-        
+        guard let imageData = UIImageJPEGRepresentation(selectedPhoto, 0.5) else {
+            print("Couldn't get JPEG representation")
+            return
+        }
         photoToUpload.contentMode = .scaleAspectFit
         photoToUpload.image = selectedPhoto
         
         uploadBar.isHidden = false
         uploadBar.progress = 0.0
-        upload(image: (selectedPhoto),
-               progressCompletion: {[unowned self] percent in
-                self.uploadBar.setProgress(percent, animated: true)
-            },
-               completion: {[unowned self] tags, colors in
-        })
-        //        self.uploadToFirebase(photo: selectedPhoto)
+        
         dismiss(animated: true)
+        uploadPhoto(photoData: imageData, poNumber: (employeeInfo?.employeeJobs[0])!)
+//        upload(image: (selectedPhoto),
+//               progressCompletion: {[unowned self] percent in
+//                self.uploadBar.setProgress(percent, animated: true)
+//        })
+        //        self.uploadToFirebase(photo: selectedPhoto)
     }
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
@@ -119,76 +122,79 @@ extension HomeView {
         self.present(actionsheet, animated: true)
     }
     
-    struct PhotoColor {
-        var red: Int?
-        var green: Int?
-        var blue: Int?
-        var colorName: String?
+    func uploadPhoto(photoData: Data, poNumber: String){
+        
+        let jsonString = "https://mb-server-app-kbradbury.c9users.io/"
+        let route = "job/1234/upload"
+        let url = URL(string: jsonString + route)!
+        var request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalAndRemoteCacheData, timeoutInterval: 10.0 * 5000)
+        request.httpMethod = "POST"
+        request.addValue("image/jpeg", forHTTPHeaderField: "mimeType")
+        request.httpBody = photoData
+        print("request body is -- \(request.httpBody)")
+        let session = URLSession.shared;
+        let task = session.dataTask(with: request) {data, response, error in
+            if error != nil {
+                print("failed to fetch JSON from database \n \(String(describing: response)) \n \(String(describing: error))")
+                return
+            } else {
+                guard let verifiedData = data else {
+                    print("could not verify data from dataTask")
+                    return
+                }
+                
+                guard let json = (try? JSONSerialization.jsonObject(with: verifiedData, options: [])) as? NSDictionary else { return }
+                
+            }
+        }
+        task.resume()
     }
     
     func upload(image: UIImage,
-                progressCompletion: @escaping (_ percent: Float) -> Void,
-                completion: @escaping (_ tags: [String], _ colors: [PhotoColor]) -> Void ) {
+                progressCompletion: @escaping (_ percent: Float) -> Void) {
         
         let address = "https://mb-server-app-kbradbury.c9users.io/job/"
         let jobNumber = String(1234) // PO - Grand and Foothill
         let url = address + jobNumber + "/upload"
         guard let photoName = employeeInfo?.employeeJobs[0] else {return}
+        let fileName = photoName + ".jpg"
         guard let imageData = UIImageJPEGRepresentation(image, 0.5) else {
             print("Couldn't get JPEG representation")
             return
         }
-//        let request = Alamofire.upload(imageData, to: url)
-//        request.validate()
-//        request.response { response in
-//            print(response)
-//        }
         
         Alamofire.upload(
             multipartFormData: { multipartFormData in
                 multipartFormData.append(imageData,
-                                         withName: photoName,
-                                         fileName: "\(photoName).jpg",
-                    mimeType: "image/jpeg")
-            },
+                                         withName: fileName,
+                                         mimeType: "image/jpg")
+                print(imageData)
+        },
             to: url,
-            headers: ["Content-Type":"image/jpeg"],
+            //            headers: ["Content-Type":"image/jpeg"],
             encodingCompletion: { encodingResult in
-                print("encoding result -- \(encodingResult)")
                 switch encodingResult {
-
+                    
                 case .success(let upload, _, _):
                     upload.uploadProgress { progress in
                         progressCompletion(Float(progress.fractionCompleted))
                     }
-                    upload.validate()
+                    //                    upload.validate()
                     upload.responseJSON { response in
                         guard response.result.isSuccess else {
                             print("error while uploading file: \(response.result.error)")
-                            completion([String](), [HomeView.PhotoColor]())
                             return
                         }
-                        guard let responseJSON = response.result.value as? [String: Any],
-                            let uploadedFiles = responseJSON["uploaded"] as? [[String: Any]],
-                            let firstFile = uploadedFiles.first,
-                            let firstFileID = firstFile["id"] as? String else {
-                                print("invalid information received from service")
-                                completion([String](), [HomeView.PhotoColor]())
-                                return
-                        }
-                        print("Content uploaded with ID: \(firstFileID)")
                         self.main.addOperation {
                             self.uploadBar.isHidden = true
                             self.confirmUpload()
                         }
-                        completion([String](), [HomeView.PhotoColor]())
                     }
-
+                    
                 case .failure(let encodingError):
                     print(encodingError)
                 }
         }
-
         )
     }
     
