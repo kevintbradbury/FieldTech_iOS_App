8034a9479af5f71f8829c85efe7ff94481cb3c36
Kevin B <kevin.t.bradbury@gmail.com>
2018-01-02 18:32:54 -0800

    built Calendar, but not displaying cells

---
+++ FieldApp/FieldApp/CalendarCell.swift
@@ -0,0 +1,15 @@
+//
+//  CalendarCell.swift
+//  FieldApp
+//
+//  Created by MB Mac 3 on 1/2/18.
+//  Copyright © 2018 Kevin Bradbury. All rights reserved.
+//
+
+import Foundation
+import JTAppleCalendar
+
+class CalendarCell: JTAppleCell {
+    @IBOutlet var dateLabel: UILabel!
+    
+}
--- FieldApp/FieldApp/ScheduleView.swift
+++ FieldApp/FieldApp/ScheduleView.swift
@@ -1,13 +1,64 @@
 //
 //  ScheduleView.swift
-//  
+//  FieldApp
 //
 //  Created by MB Mac 3 on 1/2/18.
+//  Copyright © 2018 Kevin Bradbury. All rights reserved.
 //
 
 import Foundation
 import UIKit
+import JTAppleCalendar
 
 class ScheduleView: UIViewController {
     
+    @IBOutlet weak var weekLabel: UILabel!
+    @IBOutlet weak var calendarView: JTAppleCalendarView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        calendarView.calendarDelegate = self
+        calendarView.calendarDataSource = self
+        calendarView.register(CalendarCell.self, forCellWithReuseIdentifier: "customCalendarCell")
+    }
+}
+
+extension ScheduleView: JTAppleCalendarViewDataSource, JTAppleCalendarViewDelegate {
+    
+    func calendar(_ calendar: JTAppleCalendarView, willDisplay cell: JTAppleCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
+        
+        let cell = cell as! CalendarCell
+        
+        cell.dateLabel.text = cellState.text
+    }
+    
+    func configureCalendar(_ calendar: JTAppleCalendarView) -> ConfigurationParameters {
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MM dd yyyy"
+        formatter.timeZone = Calendar.current.timeZone
+        formatter.locale = Calendar.current.locale
+        
+        let startDate = formatter.date(from: "01 01 2018")
+        let endDate = formatter.date(from: "12 31 2018")
+        
+        let parameters = ConfigurationParameters(startDate: startDate!,
+                                                 endDate: endDate!,
+                                                 numberOfRows: 6,
+                                                 calendar: Calendar.current,
+                                                 generateInDates: .forAllMonths,
+                                                 generateOutDates: .tillEndOfGrid,
+                                                 firstDayOfWeek: .sunday)
+        
+        return parameters
+    }
+    
+    func calendar(_ calendar: JTAppleCalendarView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTAppleCell {
+        
+        let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "customCalendarCell", for: indexPath) as! CalendarCell
+        
+//        cell.dateLabel.text = cellState.text
+        
+        return cell
+    }
 }
