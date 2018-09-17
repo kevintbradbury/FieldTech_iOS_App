ee2b80da4582b1d404fabf9d7283cb23b039842e
Kevin B <kevin.t.bradbury@gmail.com>
2018-09-17 11:16:12 -0700

    created StoresMapView for FieldSupplies Requests

---
+++ FieldApp/FieldApp/StoresMap.swift
@@ -0,0 +1,20 @@
+//
+//  StoresMap.swift
+//  FieldApp
+//
+//  Created by MB Mac 3 on 9/17/18.
+//  Copyright © 2018 Kevin Bradbury. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import MapKit
+
+
+class StoresMapView: UIViewController {
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
