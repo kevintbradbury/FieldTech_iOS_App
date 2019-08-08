2a820dfa820198ecbd03bc16aa4eef3075335c27
Kevin B <kevin.t.bradbury@gmail.com>
2019-08-07 17:28:48 -0700

    Working on UI Testing

--- FieldApp/FieldAppUITests/FieldAppUITests.swift
+++ FieldApp/FieldAppUITests/FieldAppUITests.swift
@@ -13,19 +13,18 @@ import Alamofire
 @testable import FieldApp
 
 class FieldAppUITests: XCTestCase {
-     var app: XCUIApplication!
+    var app: XCUIApplication!
+    var viewController: UIViewController!
     
     override func setUp() {
         super.setUp()
-        app = XCUIApplication()
-        app.launch()
         // Put setup code here. This method is called before the invocation of each test method in the class.
         
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
         XCUIApplication().launch()
-
+        
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
     
@@ -39,82 +38,14 @@ class FieldAppUITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    class ChangeOrderVwTest: XCTestCase {
+    func testHomeButton() {
         
-        func testCOvw() {
-            
-            let app = XCUIApplication()
-            let window = app.children(matching: .window).element(boundBy: 0)
-            let element = window.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element
-            element.tap()
-            element.tap()
-            
-            let element2 = window.children(matching: .other).element(boundBy: 1).children(matching: .other).element
-            element2.tap()
-            element2.children(matching: .textField).element(boundBy: 0).tap()
-            app/*@START_MENU_TOKEN@*/.keys["more"]/*[[".keyboards",".keys[\"more, numbers\"]",".keys[\"more\"]"],[[[-1,2],[-1,1],[-1,0,1]],[[-1,2],[-1,1]]],[0]]@END_MENU_TOKEN@*/.tap()
-            XCUIDevice.shared.orientation = .portrait
-            
-            let key = app/*@START_MENU_TOKEN@*/.keys["2"]/*[[".keyboards.keys[\"2\"]",".keys[\"2\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key.tap()
-            
-            let key2 = app/*@START_MENU_TOKEN@*/.keys["0"]/*[[".keyboards.keys[\"0\"]",".keys[\"0\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key2.tap()
-            key.tap()
-            key2.tap()
-            key2.tap()
-            
-            let deleteKey = app/*@START_MENU_TOKEN@*/.keys["delete"]/*[[".keyboards.keys[\"delete\"]",".keys[\"delete\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            deleteKey.tap()
-            deleteKey.tap()
-            deleteKey.tap()
-            deleteKey.tap()
-            key2.tap()
-            key2.tap()
-            key.tap()
-            key.tap()
-            key2.tap()
-            key2.tap()
-            app.tables/*@START_MENU_TOKEN@*/.staticTexts["2020 - Foster and Bellflower"]/*[[".cells.staticTexts[\"2020 - Foster and Bellflower\"]",".staticTexts[\"2020 - Foster and Bellflower\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            
-            let tKey = app/*@START_MENU_TOKEN@*/.keys["t"]/*[[".keyboards.keys[\"t\"]",".keys[\"t\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            tKey.tap()
-            app/*@START_MENU_TOKEN@*/.keys["o"]/*[[".keyboards.keys[\"o\"]",".keys[\"o\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            
-            let pKey = app/*@START_MENU_TOKEN@*/.keys["p"]/*[[".keyboards.keys[\"p\"]",".keys[\"p\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            pKey.tap()
-            pKey.tap()
-            element2.children(matching: .textField).element(boundBy: 2).tap()
-            app/*@START_MENU_TOKEN@*/.keys["m"]/*[[".keyboards.keys[\"m\"]",".keys[\"m\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            
-            let aKey = app/*@START_MENU_TOKEN@*/.keys["a"]/*[[".keyboards.keys[\"a\"]",".keys[\"a\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            aKey.tap()
-            tKey.tap()
-            
-            let eKey = app/*@START_MENU_TOKEN@*/.keys["e"]/*[[".keyboards.keys[\"e\"]",".keys[\"e\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            eKey.tap()
-            
-            let rKey = app/*@START_MENU_TOKEN@*/.keys["r"]/*[[".keyboards.keys[\"r\"]",".keys[\"r\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            rKey.tap()
-            aKey.tap()
-            
-            let lKey = app/*@START_MENU_TOKEN@*/.keys["l"]/*[[".keyboards.keys[\"l\"]",".keys[\"l\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            lKey.tap()
-            lKey.tap()
-            element2.children(matching: .textField).element(boundBy: 3).tap()
-            app/*@START_MENU_TOKEN@*/.keys["g"]/*[[".keyboards.keys[\"g\"]",".keys[\"g\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            rKey.tap()
-            eKey.tap()
-            app/*@START_MENU_TOKEN@*/.keys["y"]/*[[".keyboards.keys[\"y\"]",".keys[\"y\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
-            element2.children(matching: .textField).element(boundBy: 4).tap()
-            
-            let key3 = app/*@START_MENU_TOKEN@*/.keys["5"]/*[[".keyboards.keys[\"5\"]",".keys[\"5\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/
-            key3.tap()
-            key3.tap()
-            app.staticTexts["Color/Spec"].tap()
-            XCUIDevice.shared.orientation = .faceUp
-            
-        }
+        let app = XCUIApplication()
+        let window = app.children(matching: .window).element(boundBy: 0)
+        let element = window.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 0).children(matching: .other).element
+        element.tap()
+        element.tap()
     }
+    
 }
 
